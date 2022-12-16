a53891d57e77b452ce0e7aa223b213d9e683f35f
Alexander Nemtsev <55802702+AlexNemtsev@users.noreply.github.com>
2022-12-16 05:34:25 +0000

    feat: create bundle

---
+++ docs/index.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";console.log("Hello World!")})();
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJtYXBwaW5ncyI6Im1CQUFBQSxRQUFRQyxJQUFJLGUiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9vbmxpbmUtc3RvcmUvLi9zcmMvaW5kZXgudHMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ0hlbGxvIFdvcmxkIScpO1xuXG5sZXQgYSA9IDE7XG5hKys7XG4iXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJzb3VyY2VSb290IjoiIn0=
