df6617ada99054b3fbad78cfac6bd45b35a453c1
Alexander Nemtsev <55802702+AlexNemtsev@users.noreply.github.com>
2022-12-19 07:11:35 +0000

    feat: add data loader class

--- src/data-handler.ts
+++ src/data-handler.ts
@@ -12,9 +12,31 @@ type product = {
   images: string[];
 };
 
-type response = {
+type responseData = {
   products: product[];
   total: number;
   skip: number;
   limit: number;
 };
+
+class DataLoader {
+  errorHandler(res: Response): Response | never {
+    if (res.ok) return res;
+    else {
+      if (res.status === 401 || res.status === 404) {
+        console.log(
+          `Sorry, but there is ${res.status} error: ${res.statusText}`,
+        );
+      }
+      throw Error(res.statusText);
+    }
+  }
+
+  fetchProductsData(): Promise<void | responseData> {
+    return fetch('https://dummyjson.com/products?limit=100')
+      .then((res) => this.errorHandler(res))
+      .then((res) => res.json())
+      .then((data) => data as responseData)
+      .catch((err) => console.error(err));
+  }
+}
