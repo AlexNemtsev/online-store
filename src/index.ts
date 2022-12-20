bce8c81dcd81b772965267617fb4731eae091d0a
Alexander Nemtsev <mrcrazy696@gmail.com>
2022-12-20 17:12:53 +0300

    feat: add check for loader

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+import DataLoader from './data-loader';
+
+const getData = async () => {
+  console.log(await DataLoader.fetchProductsData());
+  console.log(5);
+};
+
+getData();
