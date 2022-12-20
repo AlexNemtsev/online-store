1b528ddc92f59a905be5046a394d48439ebc3526
Alexander Nemtsev <55802702+AlexNemtsev@users.noreply.github.com>
2022-12-20 07:24:20 +0000

    refactor: move type product to a distinct file

--- src/data-loader.ts
+++ src/data-loader.ts
@@ -1,16 +1,4 @@
-type product = {
-  id: number;
-  title: string;
-  description: string;
-  price: number;
-  discountPercentage: number;
-  rating: number;
-  stock: number;
-  brand: string;
-  category: string;
-  thumbnail: string;
-  images: string[];
-};
+import product from "./interfaces/product";
 
 type responseData = {
   products: product[];
