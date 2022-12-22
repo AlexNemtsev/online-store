7057d520f0f2895ec8e2b0b557c0f1423fd71e68
Alexander Nemtsev <mrcrazy696@gmail.com>
2022-12-22 07:49:00 +0300

    feat: add func

---
+++ src/view/grid-view.ts
@@ -0,0 +1,10 @@
+import product from '../interfaces/product';
+
+const fillCard = (product: product, template: HTMLElement) => {
+  const card = template.children[0];
+};
+
+const gridView = (products: product[]): void => {
+  const parentElement = document.querySelector('.main');
+  const template = document.getElementById('card-template');
+};
