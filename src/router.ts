0ae0f82207c1614661671adc785c23f50b0c3fb4
Alexander Nemtsev <55802702+AlexNemtsev@users.noreply.github.com>
2023-01-09 09:34:15 +0000

    fix: fix routing

--- src/router.ts
+++ src/router.ts
@@ -56,7 +56,7 @@ class Router {
       CartPageView.draw();
     } else if (path.split('-')[0] === '/product') {
       ProductPageView.fillPageTemplate(
-        MainPageView.allProducts[Number(path.split('-')[1]) - 1],
+        MainPageView.allProducts[Number(path.split('-')[1]) - 2],
       );
     } else {
       PageNotFoundView.draw();
