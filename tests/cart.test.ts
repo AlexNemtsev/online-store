b93aadb525b2b5af4c1bad37a455cf3b5b0bfd89
Alexander Nemtsev <55802702+AlexNemtsev@users.noreply.github.com>
2023-01-13 09:20:27 +0000

    refactor: create data files

--- tests/cart.test.ts
+++ tests/cart.test.ts
@@ -1,37 +1,16 @@
 import { beforeEach, describe, expect, it } from '@jest/globals';
 import Cart from '../src/cart';
 import CartElement from '../src/interfaces/cart-element';
-import Product from '../src/interfaces/product';
+import items from './test-data/products';
+import cartElements from './test-data/cart-elements';
 import LocalStorageMock from './mocks/local-storage-mock';
 
 beforeEach(async () => (globalThis.localStorage = new LocalStorageMock()));
 
-describe('When add a product to cart', () => {
-  it('should contain added product', () => {
-    const item: Product = {
-      id: 1,
-      title: 'iPhone 9',
-      description: 'An apple mobile which is nothing like apple',
-      price: 549,
-      discountPercentage: 12.96,
-      rating: 4.69,
-      stock: 94,
-      brand: 'Apple',
-      category: 'smartphones',
-      thumbnail: 'https://i.dummyjson.com/data/products/1/thumbnail.jpg',
-      images: [
-        'https://i.dummyjson.com/data/products/1/1.jpg',
-        'https://i.dummyjson.com/data/products/1/2.jpg',
-        'https://i.dummyjson.com/data/products/1/3.jpg',
-        'https://i.dummyjson.com/data/products/1/4.jpg',
-        'https://i.dummyjson.com/data/products/1/thumbnail.jpg',
-      ],
-    };
-
-    Cart.addToCart(item);
-    const expected: CartElement[] = [
-      { id: item.id, amount: 1, price: item.price },
-    ];
+describe('When add products to cart', () => {
+  it('should contain added products', () => {
+    items.forEach((item) => Cart.addToCart(item))
+    const expected: CartElement[] = cartElements;
 
     expect(Cart.cart).toEqual(expected);
   });
---
+++ tests/test-data/cart-elements.ts
@@ -0,0 +1,12 @@
+import CartElement from '../../src/interfaces/cart-element';
+import items from './products';
+
+const cartElements: CartElement[] = items.map((item) => {
+  return {
+    id: item.id,
+    amount: 1,
+    price: item.price,
+  };
+});
+
+export default cartElements;
